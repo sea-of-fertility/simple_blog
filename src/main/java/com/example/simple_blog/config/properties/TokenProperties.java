145cf74ea9e662dd3d26887f058931550fd05f32
LetgoMoveNotLate <hj981203@gmail.com>
2024-04-22 17:01:19 +0900

    FIX: get할때 TimeMillis로 변경하는 것이 아닌 생성자를  호출할 때 TimeMillis로 변경하도록 수정했다.

--- src/main/java/com/example/simple_blog/config/properties/TokenProperties.java
+++ src/main/java/com/example/simple_blog/config/properties/TokenProperties.java
@@ -1,27 +1,24 @@
 package com.example.simple_blog.config.properties;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
 public class TokenProperties {
 
-    @Value("${security.token.access.expirationMinutes}")
-    private Long accessTokenExpirationMinutes;
-
-    @Value("${security.token.refresh.expirationDays}")
-    private Long refreshTokenExpirationDays;
-
+    private final Long accessTokenExpirationMinutes;
+    private final Long refreshTokenExpirationDays;
     public static final String ACCESS_TOKEN_NAME = "access";
-
     public static final String REFRESH_TOKEN_NAME = "refresh";
 
 
-    public Long getAccessTokenExpirationMinutes() {
-        return accessTokenExpirationMinutes * 60000;
-    }
 
-    public Long getRefreshTokenExpirationDays() {
-        return refreshTokenExpirationDays * 86400000;
+    public TokenProperties(@Value("${security.token.access.expirationMinutes}")Long accessTokenExpirationMinutes,
+                           @Value("${security.token.refresh.expirationDays}") Long refreshTokenExpirationDays)
+    {
+        this.accessTokenExpirationMinutes = accessTokenExpirationMinutes * 60000;
+        this.refreshTokenExpirationDays = refreshTokenExpirationDays * 86400000;
     }
 }
