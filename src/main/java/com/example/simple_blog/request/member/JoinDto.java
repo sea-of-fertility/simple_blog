f73fe3fae789febd8a450c55b35229482766f862
LetgoMoveNotLate <hj981203@gmail.com>
2024-04-24 23:10:30 +0900

    FIX: 역직렬화가 되지 않는 에러를 수정했다.
    body: 기본 생성자가 없어서 역질렬화가 되지 않아 에러가 발생했다.

--- src/main/java/com/example/simple_blog/request/member/JoinDto.java
+++ src/main/java/com/example/simple_blog/request/member/JoinDto.java
@@ -6,29 +6,30 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.Getter;
 
 import static com.example.simple_blog.service.member.JoinValidator.*;
-
+@Getter
 public class  JoinDto {
+
 
     @NotBlank
     @Pattern(regexp = EMAIL_PATTERN, message = "이메일 형식에 맞게 설졍해주세요")
-    private final String address;
+    private String address;
 
     @NotBlank
     @Size(min = 1, max = 8)
     @Pattern(regexp = NICKNAME_PATTERN, message = "길이는 1~8, 특수문자는 사용하지 못합니다.")
-    private final String memberNickName;
+    private String memberNickName;
 
     @NotBlank
     @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "특수문자는 사용할 수 없습니다.")
-    private final String memberName;
+    private String memberName;
 
     @NotBlank
     @Pattern(regexp = PASSWORD_PATTERN, message = "형식에 맞게 설졍해주세요")
     @Size(min = 8, max = 13)
-    private final String password;
+    private String password;
 
 
     @Builder
