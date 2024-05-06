95d1ddcab702c58d254441d05314d60a7a3c90ec
LetgoMoveNotLate <hj981203@gmail.com>
2024-05-06 20:49:18 +0900

    FEAT: 조회 수정 기능 추가

--- src/main/java/com/example/simple_blog/repository/PostRepository.java
+++ src/main/java/com/example/simple_blog/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package com.example.simple_blog.repository;
 
 import com.example.simple_blog.domain.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+
+    Page<Post> findAll(Pageable pageable);
 }
--- src/main/java/com/example/simple_blog/service/post/PostService.java
+++ src/main/java/com/example/simple_blog/service/post/PostService.java
@@ -1,10 +1,12 @@
 package com.example.simple_blog.service.post;
 
 import com.example.simple_blog.domain.post.Post;
-import com.example.simple_blog.exception.member.login.MemberNotFoundException;
+import com.example.simple_blog.exception.post.PostNotFoundException;
 import com.example.simple_blog.repository.PostRepository;
-import com.example.simple_blog.service.member.MemberService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +14,32 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepository postRepository;
-    private final MemberService memberService;
 
-    public void save(Long memberId, Post post) throws MemberNotFoundException {
-        postRepository.save(post);
+    public Long save(Post post){
+        return postRepository.save(post).getId();
+    }
+
+    public Post get(Long id) {
+        return postRepository.findById(id).orElseThrow(PostNotFoundException::new);
+    }
+
+    public void delete(Long postId) {
+        postRepository.deleteById(postId);
+
+    }
+
+    public Post findById(Long id) {
+        return postRepository.findById(id).orElseThrow(PostNotFoundException::new);
+    }
+
+    public Page<Post> getPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public Post edit(Long id, Post edite) {
+        Post post = postRepository.findById(id).orElseThrow(PostNotFoundException::new);
+        post.edit(edite);
+        return post;
     }
 }
