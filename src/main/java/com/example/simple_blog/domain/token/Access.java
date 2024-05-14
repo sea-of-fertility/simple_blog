f6e12c9a8333845278e930d75582ebb39128f075
LetgoMoveNotLate <hj981203@gmail.com>
2024-05-14 17:22:37 +0900

    FEAT: access token black list에 등록하기 위한 기능

--- src/main/java/com/example/simple_blog/domain/token/Access.java
+++ src/main/java/com/example/simple_blog/domain/token/Access.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.core.index.Indexed;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@RedisHash(value = "access", timeToLive = 10)
+@RedisHash(value = "access", timeToLive = 1)
 public class Access {
 
     @Id
